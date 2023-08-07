834594add1541d6f730a1deb53f476a057441c84
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-08-07 17:45:06 -0500

    update

--- index.php
+++ index.php
@@ -52,45 +52,43 @@ if (!empty($_SESSION['active'])) {
 <div class="container" id="container">
 	<div class="form-container sign-up-container">
 		<form action="#">
-			<h1>Create Account</h1>
+			<h1>Contactanos</h1>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
-			<span>or use your email for registration</span>
+			
 			<input type="text" placeholder="Name" />
 			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
+			
+			<button>Contactanos</button>
 		</form>
 	</div>
 	<div class="form-container sign-in-container">
-		<form action="#">
-			<h1>Sign in</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
+    <form class="login" action="" method="post">
+			<h1>Inicia sesión</h1>
+			<input type="text" id="name" name="usuario" placeholder="Usuario" />
+			<input type="password" id="apellido" name="clave" placeholder="Contraseña" />
+			<a href="#">¿Olvidaste tu contraseña?</a>
+      <input type="submit" value="INGRESAR" id="ingresar" class="button">
+
+      <label class="alert" id="alerta">
+        <?php echo isset($alert) ? $alert : ''; ?>
+      </label>
 		</form>
 	</div>
 	<div class="overlay-container">
 		<div class="overlay">
 			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
+				<h1>Hola</h1>
+				<p>Indicanos tus datos y te enviaremos la información</p>
 				<button class="ghost" id="signIn">Sign In</button>
 			</div>
 			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+				<h1>¿Eres nuevo?</h1>
+				<p>Bienvenido a nuestro sistema....</p>
+				<button class="ghost" id="signUp">Contactanos</button>
 			</div>
 		</div>
 	</div>
