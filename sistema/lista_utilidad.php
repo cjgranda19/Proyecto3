789f7bf5b2d33f35077f0a128a53928d0a1da649
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-08-12 00:41:50 -0500

    Update lista_utilidad.php

--- sistema/lista_utilidad.php
+++ sistema/lista_utilidad.php
@@ -1,10 +1,11 @@
 <?php
-	session_start();
-	include "../conexion.php";
+session_start();
+include "../conexion.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<?php include "includes/scripts.php"; ?>
@@ -12,12 +13,12 @@
 </head>
 
 <style>
-	#container h1{
+	#container h1 {
 		font-size: 35px;
 		display: inline-block;
 	}
 
-	.btn_new{
+	.btn_new {
 		display: inline-block;
 		background: rgb(107, 2, 46);
 		color: #fff;
@@ -34,14 +35,15 @@
 		transform: scale(1.02);
 	}
 
-	form{
+	form {
 		background: #fff;
 		margin: auto;
 		padding: 20px 50px;
 		border: 1px solid #d1d1d1;
 	}
 
-	input, select{
+	input,
+	select {
 		display: block;
 		width: 100%;
 		font-size: 13pt;
@@ -51,41 +53,41 @@
 	}
 
 
-	table{
+	table {
 		border-collapse: collapse;
 		font-size: 12pt;
 		font-family: 'GothamBook';
 		width: 100%;
 	}
 
-	table th{
+	table th {
 		text-align: left;
 		padding: 10px;
 		background: rgb(192, 27, 96);
 		color: #fff;
 	}
 
-	table tr:nth-child(odd){
+	table tr:nth-child(odd) {
 		background: #fff;
 	}
 
-	table td{
+	table td {
 		padding: 10px;
 	}
 
-	.link_edit{
+	.link_edit {
 		color: #0ca4ce;
 	}
 
-	.link_delete{
+	.link_delete {
 		color: #f26b6b;
 	}
 
-	.link_add{
+	.link_add {
 		color: #64b13c;
 	}
 
-	.paginador ul{
+	.paginador ul {
 		padding: 15px;
 		list-style: none;
 		background: #fff;
@@ -97,7 +99,8 @@
 		justify-content: flex-end;
 	}
 
-	.paginador a, .pageSelected{
+	.paginador a,
+	.pageSelected {
 		color: #428bca;
 		border: 1px solid #ddd;
 		display: 5px;
@@ -107,17 +110,17 @@
 		width: 35px;
 	}
 
-	.paginador a:hover{
+	.paginador a:hover {
 		background: #ddd;
 	}
 
-	.pageSelected{
+	.pageSelected {
 		color: #fff;
 		background: #428bca;
 		border: 1px solid #428bca;
 	}
 
-	.form_search{
+	.form_search {
 		display: -webkit-flex;
 		display: -moz-flex;
 		display: -ms-flex;
@@ -129,7 +132,7 @@
 		border-radius: 10px;
 	}
 
-	.form_search .btn_search{
+	.form_search .btn_search {
 		background: rgb(107, 2, 46);
 		color: #fff;
 		padding: 0 20px;
@@ -146,12 +149,11 @@
 		transform: scale(1.02);
 	}
 
-	.img_de_producto img{
+	.img_de_producto img {
 		width: 80px;
 		height: auto;
 		margin: auto;
 	}
-
 </style>
 
 <body>
@@ -174,7 +176,7 @@
 				<th>Proveedor</th>
 				<th>Acciones</th>
 			</tr>
-		<?php
+			<?php
 
 			//paginador
 			$sql_registe = mysqli_query($conection, "SELECT COUNT(*) as total_registro FROM inventario WHERE estatus = 1 ");
@@ -183,64 +185,93 @@
 
 			$por_pagina = 10;
 
-			if(empty($_GET['pagina'])){
+			if (empty($_GET['pagina'])) {
 				$pagina = 1;
-			}else{
+			} else {
 				$pagina = $_GET['pagina'];
 			}
 
-			$desde= ($pagina-1) * $por_pagina;
+			$desde = ($pagina - 1) * $por_pagina;
 			$total_paginas = ceil($total_registro / $por_pagina);
 
-			$query = mysqli_query($conection, "SELECT p.cod_inventario, p.nombre_inventario, p.cantidad_inventario, p.medida_inv, pr.proveedor FROM inventario p INNER JOIN proveedor pr ON p.proveedor = pr.codproveedor WHERE p.estatus = 1 ORDER BY p.cod_inventario ASC LIMIT $desde,$por_pagina");
+			$query = "SELECT p.cod_inventario, p.nombre_inventario, p.cantidad_inventario, p.medida_inv, pr.proveedor
+          FROM inventario p
+          INNER JOIN proveedor pr ON p.proveedor = pr.codproveedor
+          WHERE p.estatus = 1
+          ORDER BY p.cod_inventario ASC
+          LIMIT $desde, $por_pagina";
+
+			$result = mysqli_query($conection, $query);
+
+			if (!$result) {
+				die("Error en la consulta: " . mysqli_error($conection));
+			}
+
+			// Resto del código para mostrar los resultados en la tabla
+			
 
 			mysqli_close($conection);
 			$result = mysqli_num_rows($query);
 
-			if($result>0){
-				while ($data = mysqli_fetch_array($query)){
-		?>
-			<tr>
-				<td><?php echo $data['cod_inventario']; ?></td>
-				<td><?php echo $data['nombre_inventario']; ?></td>
-				<td><?php echo $data['cantidad_inventario']; ?></td>
-				<td><?php echo $data['medida_inv']; ?></td>				
-				<td><?php echo $data['proveedor']; ?></td>
+			if ($result > 0) {
+				while ($data = mysqli_fetch_array($query)) {
+					?>
+					<tr>
+						<td>
+							<?php echo $data['cod_inventario']; ?>
+						</td>
+						<td>
+							<?php echo $data['nombre_inventario']; ?>
+						</td>
+						<td>
+							<?php echo $data['cantidad_inventario']; ?>
+						</td>
+						<td>
+							<?php echo $data['medida_inv']; ?>
+						</td>
+						<td>
+							<?php echo $data['proveedor']; ?>
+						</td>
 
-				<?php if($_SESSION['rol'] == 1){ ?>
-				<td>
-					<a class="link_edit" href="editar_inventario.php?id=<?php echo $data['cod_inventario']; ?>">Editar</a>
-					<a class="link_delete" href="eliminar_confirmar_inventario.php?id=<?php echo $data['cod_inventario']; ?>"> Eliminar</a>
-				</td>
-				<?php } ?>
-			</tr>
+						<?php if ($_SESSION['rol'] == 1) { ?>
+							<td>
+								<a class="link_edit"
+									href="editar_inventario.php?id=<?php echo $data['cod_inventario']; ?>">Editar</a>
+								<a class="link_delete"
+									href="eliminar_confirmar_inventario.php?id=<?php echo $data['cod_inventario']; ?>"> Eliminar</a>
+							</td>
+						<?php } ?>
+					</tr>
 
-		<?php
+					<?php
 				}
 			}
-		?>
-			
+			?>
+
 		</table>
 
 		<div class="paginador">
 			<ul>
 				<?php
-					if($pagina != 1){
-				?>
-					<li><a href="?pagina=<?php echo 1; ?>">|<</a></li>
-					<li><a href="?pagina=<?php echo $pagina-1; ?>"><<</a></li>
-				<?php
+				if ($pagina != 1) {
+					?>
+					<li><a href="?pagina=<?php echo 1; ?>">|<< /a>
+					</li>
+					<li><a href="?pagina=<?php echo $pagina - 1; ?>">
+							<<< /a>
+					</li>
+					<?php
+				}
+				for ($i = 1; $i <= $total_paginas; $i++) {
+					if ($i == $pagina) {
+						echo '<li class="pageSelected">' . $i . '</li>';
+					} else {
+						echo '<li><a href="?pagina=' . $i . '">' . $i . '</a></li>';
 					}
-					for ($i=1; $i <= $total_paginas; $i++){
-						if($i==$pagina){
-							echo '<li class="pageSelected">'.$i.'</li>';
-						}else{
-							echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
-						}
-					}
+				}
 
-					if($pagina != $total_paginas){
-				?>	
+				if ($pagina != $total_paginas) {
+					?>
 					<li><a href="?pagina=<?php echo $pagina + 1; ?>">>></a></li>
 					<li><a href="?pagina=<?php echo $total_paginas; ?> ">>|</a></li>
 				<?php } ?>
@@ -250,4 +281,5 @@
 	</section>
 	<?php include "includes/footer.php"; ?>
 </body>
+
 </html>
