a8f9069c0c666eb5f4150bf959b2e3215f556ca8
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-08-22 22:23:02 -0500

    Update lista_usuarios.php

--- sistema/lista_usuarios.php
+++ sistema/lista_usuarios.php
@@ -1,15 +1,16 @@
 <?php
-	session_start();
-	if($_SESSION['rol'] != 1){
-		header("location: ./");
-	}
-	include "../conexion.php";
+session_start();
+if ($_SESSION['rol'] != 1) {
+	header("location: ./");
+}
+include "../conexion.php";
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<?php include "includes/scripts.php"; ?>
@@ -17,6 +18,7 @@
 	<link href="css/lista_usuarios.css" rel="stylesheet" type="text/css">
 
 </head>
+
 <body>
 	<?php include "includes/header.php"; ?>
 	<section id="container">
@@ -37,72 +39,83 @@
 				<th>Rol</th>
 				<th>Acciones</th>
 			</tr>
-		<?php
+			<?php
 
-			//paginador
 			$sql_registre = mysqli_query($conection, "SELECT COUNT(*) as total_registro FROM usuario WHERE estatus = 1 ");
 			$result_registre = mysqli_fetch_array($sql_registre);
 			$total_registro = $result_registre['total_registro'];
 
-			$por_pagina = 8;
+			$por_pagina = 10;
 
-			if(empty($_GET['pagina'])){
+			if (empty($_GET['pagina'])) {
 				$pagina = 1;
-			}else{
+			} else {
 				$pagina = $_GET['pagina'];
 			}
 
-			$desde= ($pagina-1) * $por_pagina;
+			$desde = ($pagina - 1) * $por_pagina;
 			$total_paginas = ceil($total_registro / $por_pagina);
 
 			$query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY idusuario ASC LIMIT $desde,$por_pagina");
 
 			mysqli_close($conection);
 			$result = mysqli_num_rows($query);
-			if($result>0){
-				while ($data = mysqli_fetch_array($query)){
-		?>
-			<tr>
-				<td><?php echo $data['idusuario']; ?></td>
-				<td><?php echo $data['nombre']; ?></td>
-				<td><?php echo $data['correo']; ?></td>
-				<td><?php echo $data['usuario']; ?></td>
-				<td><?php echo $data['rol']; ?></td>
-				<td>
-					<a class="link_edit" href="editar_usuario.php?id=<?php echo $data['idusuario']; ?>"><i class="fa-solid fa-pen-to-square"></i> Editar</a>
+			if ($result > 0) {
+				while ($data = mysqli_fetch_array($query)) {
+					?>
+					<tr>
+						<td>
+							<?php echo $data['idusuario']; ?>
+						</td>
+						<td>
+							<?php echo $data['nombre']; ?>
+						</td>
+						<td>
+							<?php echo $data['correo']; ?>
+						</td>
+						<td>
+							<?php echo $data['usuario']; ?>
+						</td>
+						<td>
+							<?php echo $data['rol']; ?>
+						</td>
+						<td>
+							<a class="link_edit" href="editar_usuario.php?id=<?php echo $data['idusuario']; ?>"><i
+									class="fa-solid fa-pen-to-square"></i> Editar</a>
 
-					<?php if($data["idusuario"] != 1){ ?>
-					<a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data['idusuario']; ?>"><i class="fa-solid fa-trash"></i> Eliminar</a>
-				<?php } ?>
-				</td>
-			</tr>
+							<?php if ($data["idusuario"] != 1) { ?>
+								<a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data['idusuario']; ?>"><i
+										class="fa-solid fa-trash"></i> Eliminar</a>
+							<?php } ?>
+						</td>
+					</tr>
 
-		<?php
+					<?php
 				}
 			}
-		?>
-			
+			?>
+
 		</table>
 
 		<div class="paginador">
 			<ul>
 				<?php
-					if($pagina != 1){
-				?>
+				if ($pagina != 1) {
+					?>
 					<li><a href="?pagina=<?php echo 1; ?>"><i class="fa-solid fa-backward-step"></i></a></li>
-					<li><a href="?pagina=<?php echo $pagina-1; ?>"><i class="fa-solid fa-backward"></i></a></li>
-				<?php
+					<li><a href="?pagina=<?php echo $pagina - 1; ?>"><i class="fa-solid fa-backward"></i></a></li>
+					<?php
+				}
+				for ($i = 1; $i <= $total_paginas; $i++) {
+					if ($i == $pagina) {
+						echo '<li class="pageSelected">' . $i . '</li>';
+					} else {
+						echo '<li><a href="?pagina=' . $i . '">' . $i . '</a></li>';
 					}
-					for ($i=1; $i <= $total_paginas; $i++){
-						if($i==$pagina){
-							echo '<li class="pageSelected">'.$i.'</li>';
-						}else{
-							echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
-						}
-					}
+				}
 
-					if($pagina != $total_paginas){
-				?>	
+				if ($pagina != $total_paginas) {
+					?>
 					<li><a href="?pagina=<?php echo $pagina + 1; ?>"><i class="fa-solid fa-forward"></i></a></li>
 					<li><a href="?pagina=<?php echo $total_paginas; ?> "><i class="fa-solid fa-forward-step"></i></a></li>
 				<?php } ?>
@@ -112,4 +125,5 @@
 	</section>
 	<?php include "includes/footer.php"; ?>
 </body>
+
 </html>
