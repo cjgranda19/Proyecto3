0ad6e2a482f9f6a1dbe8339775b5374a7367e9a2
cjgranda19 <cjgranda1@espe.edu.ec>
2023-08-27 15:58:53 -0500

    Modificacion index

--- sistema/index.php
+++ sistema/index.php
@@ -9,12 +9,15 @@
 	<meta charset="UTF-8">
 	<?php include "includes/scripts.php"; ?>
 	<link rel="icon" type="image/jpg" href="img/favicon.png"/>
+	<link rel="stylesheet" href="css/estiloindex.css" type="text/css">
 	<title>Sistema Ventas</title>
 </head>
 <body>
 	<?php include "includes/header.php"; ?>
 	<section id="container">
-		<h1>Arreglar esto ta vacio</h1>
+		<img src="img/favicon.png" alt="logo de la empresa citaviso">
+		<h1>Bienvenido al sistema de Citaviso!</h1>
+		<h3>Entraste con la cuenta "<?php echo $_SESSION['user'] ?>"</h3>
 	</section>
 </body>
 </html>
