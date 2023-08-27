b046229758b57bb49f5a05b84b01a9996cfa104c
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-08-27 17:24:51 -0500

    update

--- sistema/process/process_register_client.php
+++ sistema/process/process_register_client.php
@@ -1,70 +1,79 @@
 <?php
+session_start();
+
+include "../../conexion.php";
+
+if ($_SESSION['rol'] != 1) {
+    header("location: ./");
+    exit();
+}
+
 if (!empty($_POST)) {
-    include "../../conexion.php";
 
-$alert = '';
+    $alert = '';
 
-if (empty($_POST['cedula']) || empty($_POST['nombre']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
-    $alert = '<p class="msg_error">Todos los campos son obligatorios.</p>';
-} else {
-
-    $cedula = $_POST['cedula'];
-    $nombre = $_POST['nombre'];
-    $telefono = $_POST['telefono'];
-    $direccion = $_POST['direccion'];
-    $usuario_id = $_SESSION['idUser'];
-
-    if (!isValidCI($cedula)) {
-        $alert = '<p class="msg_error">El número de cédula no es válido. Debe ser un número real</p>';
+    if (empty($_POST['cedula']) || empty($_POST['nombre']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
+        $alert = '<p class="msg_error">Todos los campos son obligatorios.</p>';
     } else {
 
-        $query = mysqli_query($conection, "SELECT * FROM cliente WHERE cedula = '$cedula' ");
-        $result = mysqli_fetch_array($query);
+        $cedula = $_POST['cedula'];
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $direccion = $_POST['direccion'];
+        $usuario_id = $_SESSION['idUser'];
 
-        if ($result > 0) {
-            $_SESSION['popup_message'] = 'El usuario ya existe.';
+        if (!isValidCI($cedula)) {
+            $alert = '<p class="msg_error">El número de cédula no es válido. Debe ser un número real</p>';
         } else {
-            $query_insert = mysqli_query($conection, "INSERT INTO cliente(cedula,nombre,telefono,direccion,usuario_id) VALUES('$cedula','$nombre','$telefono','$direccion','$usuario_id')");
 
-            if ($query_insert) {
-                $_SESSION['popup_message'] = 'Insercion exitosa';
+            $query = mysqli_query($conection, "SELECT * FROM cliente WHERE cedula = '$cedula' ");
+            $result = mysqli_fetch_array($query);
+
+            if ($result > 0) {
+                $_SESSION['popup_message'] = 'El usuario ya existe.';
             } else {
-                $_SESSION['popup_message'] = 'Error al guardar';
-            }
-            header("location: ../lista_clientes.php");
+                $query_insert = mysqli_query($conection, "INSERT INTO cliente(cedula,nombre,telefono,direccion,usuario_id) VALUES('$cedula','$nombre','$telefono','$direccion','$usuario_id')");
 
+                if ($query_insert) {
+                    $_SESSION['popup_message'] = 'Insercion exitosa';
+                } else {
+                    $_SESSION['popup_message'] = 'Error al guardar';
+                }
+                header("location: ../lista_clientes.php");
+
+            }
         }
     }
-}
-mysqli_close($conection);
-}
-
-function isValidCI($ci) {
-if (!is_numeric($ci)) {
-    return false;
+    mysqli_close($conection);
 }
 
-if (strlen($ci) !== 10) {
-    return false;
-}
-
-$total = 0;
-$coeficientes = array(2, 1, 2, 1, 2, 1, 2, 1, 2);
-
-for ($i = 0; $i < 9; $i++) {
-    $temp = $ci[$i] * $coeficientes[$i];
-    if ($temp > 9) {
-        $temp -= 9;
+function isValidCI($ci)
+{
+    if (!is_numeric($ci)) {
+        return false;
     }
-    $total += $temp;
-}
 
-$verificador = 10 - ($total % 10);
-if ($verificador === 10) {
-    $verificador = 0;
-}
+    if (strlen($ci) !== 10) {
+        return false;
+    }
 
-return $ci[9] == $verificador;
+    $total = 0;
+    $coeficientes = array(2, 1, 2, 1, 2, 1, 2, 1, 2);
+
+    for ($i = 0; $i < 9; $i++) {
+        $temp = $ci[$i] * $coeficientes[$i];
+        if ($temp > 9) {
+            $temp -= 9;
+        }
+        $total += $temp;
+    }
+
+    $verificador = 10 - ($total % 10);
+    if ($verificador === 10) {
+        $verificador = 0;
+    }
+
+    return $ci[9] == $verificador;
 }
 
 ?>
