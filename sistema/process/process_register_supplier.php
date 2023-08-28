c45aebf3d1b0015a06e141c46ceb52bc67d2716f
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-08-27 21:19:25 -0500

    Update process_register_supplier.php

--- sistema/process/process_register_supplier.php
+++ sistema/process/process_register_supplier.php
@@ -6,13 +6,10 @@ if ($_SESSION['rol'] != 1) {
     header("location: ./");
 }
 
-
 $alert = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $proveedor = $_POST['proveedor'];
-
     $cedula = $_POST['cedula'];
     $contacto = $_POST['contacto'];
     $telefono = $_POST['telefono'];
@@ -24,27 +21,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['popup_message'] = 'Complete todos los campos';
         header("location: ../lista_proveedor.php");
     } else {
-        $query_insert = mysqli_query($conection, "INSERT INTO proveedor(cedula, proveedor,contacto,correo,telefono,direccion,usuario_id) VALUES('$cedula','$proveedor','$contacto','$correo','$telefono','$direccion','$usuario_id')");
-        $result = mysqli_query($conection, $query_insert);
+        // Check if the combination of provider, contact, and phone number already exists
+        $query_check = "SELECT * FROM proveedor WHERE proveedor = '$proveedor' AND contacto = '$contacto' AND telefono = '$telefono'";
+        $result_check = mysqli_query($conection, $query_check);
 
-        if ($result) {
-            $_SESSION['popup_message'] = 'Inserción exitosa.';
+        if (mysqli_num_rows($result_check) > 0) {
+            $_SESSION['popup_message'] = 'Ya existe un registro con el mismo proveedor, contacto y número de teléfono.';
         } else {
-            $error_message = 'Error al guardar proveedor: ' . mysqli_error($conection);
-            $error_message .= "\nDatos enviados:";
-            $error_message .= "\nCédula: $cedula";
-            $error_message .= "\nProveedor: $proveedor";
-            $error_message .= "\nContacto: $contacto";
-            $error_message .= "\nTeléfono: $telefono";
-            $error_message .= "\nCorreo: $correo";
-            $error_message .= "\nDirección: $direccion";
+            $query_insert = "INSERT INTO proveedor(cedula, proveedor, contacto, correo, telefono, direccion, usuario_id) 
+                             VALUES('$cedula', '$proveedor', '$contacto', '$correo', '$telefono', '$direccion', '$usuario_id')";
+            $result = mysqli_query($conection, $query_insert);
 
-            $_SESSION['popup_message'] = $error_message;
-            echo $error_message;
+            if ($result) {
+                $_SESSION['popup_message'] = 'Inserción exitosa.';
+            } else {
+                $error_message = 'Error al guardar proveedor: ' . mysqli_error($conection);
+                $_SESSION['popup_message'] = $error_message;
+            }
         }
         header("location: ../lista_proveedor.php");
-        mysqli_close($conection);
     }
+    mysqli_close($conection);
 }
 
 echo $alert;
